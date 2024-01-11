577e245079aaa9a9f30a61bb747be75444137989
Madhuri Pathak <madhuripathak8957940532@gmail.com>
2024-01-11 15:42:52 +0530

    modells

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,11 +62,6 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
-
-
-
-
-
 userSchema.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
